4b10cfe7c9be6ab6fa12446b94ebf68f9b492768
Prasanna Sahu <prasannasahu4u@gmail.com>
2024-02-19 20:28:47 +0530

    First Commit

--- src/main/java/com/heapsteep/JpaH2MavenApplication.java
+++ src/main/java/com/heapsteep/JpaH2MavenApplication.java
@@ -5,11 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-//@EnableCaching
+@EnableCaching
 public class JpaH2MavenApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JpaH2MavenApplication.class, args);
 	}
-
 }
--- src/main/java/com/heapsteep/config/MyConfig.java
+++ src/main/java/com/heapsteep/config/MyConfig.java
@@ -1,42 +1,18 @@
 package com.heapsteep.config;
 
-import com.heapsteep.model.Person;
-import org.ehcache.config.CacheConfiguration;
-import org.ehcache.config.builders.CacheConfigurationBuilder;
-import org.ehcache.config.builders.ExpiryPolicyBuilder;
-import org.ehcache.config.builders.ResourcePoolsBuilder;
-import org.ehcache.config.units.MemoryUnit;
-import org.ehcache.jsr107.Eh107Configuration;
-import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.boot.autoconfigure.cache.RedisCacheManagerBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.cache.CacheManager;
-import javax.cache.Caching;
-import javax.cache.spi.CachingProvider;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import java.time.Duration;
 
 @Configuration
-@EnableCaching
 public class MyConfig {
 
     @Bean
-    public CacheManager EhcacheManager() {
-
-        CacheConfiguration<String, Person> cachecConfig = CacheConfigurationBuilder
-                .newCacheConfigurationBuilder(String.class,
-                        Person.class,
-                        ResourcePoolsBuilder.newResourcePoolsBuilder()
-                                .offheap(10, MemoryUnit.MB)
-                                .build())
-                .withExpiry(ExpiryPolicyBuilder.timeToIdleExpiration(Duration.ofSeconds(10)))
-                .build();
-
-        CachingProvider cachingProvider = Caching.getCachingProvider();
-        CacheManager cacheManager = cachingProvider.getCacheManager();
-
-        javax.cache.configuration.Configuration<String, Person> configuration = Eh107Configuration.fromEhcacheCacheConfiguration(cachecConfig);
-        cacheManager.createCache("cacheStore", configuration);
-        return cacheManager;
+    public RedisCacheManagerBuilderCustomizer redisCacheManagerBuilderCustomizer() {
+        return (builder) -> builder
+                .withCacheConfiguration("cacheStore",
+                        RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofMinutes(5)));
     }
 }
